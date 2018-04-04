35e140fed40d714a760706ef81bb144b20a55c2d
carly harper <carlychastainharper@gmail.com>
2018-04-03 20:21:44 -0400

    first commit

---
+++ tictactoe.js
@@ -0,0 +1,26 @@
+/*
+1st step: state 
+= variables to define the universe
+
+2nd step: action 
+= functions that will change the universe
+
+3rd step: actually doing the actions (calling)
+= invocations
+
+
+var board = [[null, null, null],
+            [null, null, null],
+            [null, null, null]];
+
+var whosTurn = "x"
+
+*/
+
+console.log('script loaded');
+
+// 1. write a variable that creates an empty tic-tac-toe
+
+var board = [[null, null, null],
+            [null, null, null],
+            [null, null, null]];
